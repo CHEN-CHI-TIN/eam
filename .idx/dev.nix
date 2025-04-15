760a1a28e7ad52f123b473627f4b8cd8482b9a97
timmy.ct.chen <timmy.ct.chen@foxconn.com>
2025-04-15 12:35:12 +0000

    深呼吸

---
+++ .idx/dev.nix
@@ -0,0 +1,66 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.python311Packages.pip
+    # pkgs.python311
+    # pkgs.go
+    # pkgs.nodePackages.nodemon
+  ];
+  services.docker.enable = true;
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-python.python"
+      "ms-python.debugpy"
+      "ms-python.vscode-pylance"
+      "ms-python.isort"
+      "ms-toolsai.jupyter"
+      "ms-toolsai.jupyter-keymap"
+      "ms-toolsai.vscode-jupyter-cell-tags"
+      "ms-toolsai.jupyter-renderers"      
+      "ms-toolsai.vscode-jupyter-slideshow"
+      "charliermarsh.ruff"
+      "ms-python.black-formatter"
+      "enkia.tokyo-night"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };   
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+        formatter = "docker build -t tmy-img:lts ./.tmy/formatter-watch && docker run --rm -d --name tmy-img-ocr tmy-img:lts";
+      };
+    };
+  };
+}
